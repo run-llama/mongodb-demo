6552f3efa9f0cd41096689aed3d1bb22ddea8f05
Laurie Voss <github@seldo.com>
2023-10-29 17:59:28 -0700

    Cors header for API, env variables for form

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,11 +1,15 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS, cross_origin
 app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
 
 @app.route('/process_form', methods=['POST'])
+@cross_origin()
 def process_form():
     query = request.form.get('query')
     if query is not None:

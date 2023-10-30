f9c2d18a866a7fde492c7893511d47bbfd0697d1
Laurie Voss <github@seldo.com>
2023-10-29 18:02:03 -0700

    Submit form to correct env var

--- next_app/app/page.js
+++ next_app/app/page.js
@@ -1,11 +1,43 @@
-import Image from 'next/image'
+'use client'
+
 import styles from './page.module.css'
+import React, { useState } from 'react';
 
 export default function Home() {
+  const [query, setQuery] = useState('');
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    const formData = new FormData();
+    formData.append('query', query);
+
+    const response = await fetch(process.env.NEXT_PUBLIC_API_HOST + '/process_form', {
+      method: 'POST',
+      body: formData,
+    });
+
+    if (response.ok) {
+      const responseData = await response.json();
+      console.log(responseData);
+    } else {
+      console.error('Failed to submit:', response.statusText);
+    }
+  };
+
   return (
     <main className={styles.main}>
       <div>
         <h1>Sup.</h1>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            name="query"
+            value={query}
+            onChange={(e) => setQuery(e.target.value)}
+          />
+          <button type="submit">Submit</button>
+        </form>
       </div>
     </main>
   )

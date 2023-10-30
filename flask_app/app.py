1a86013008e0b61f02b4521bfc5a9dcc5641518e
Laurie Voss <github@seldo.com>
2023-10-29 17:31:41 -0700

    Basic next app

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,6 +1,15 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+@app.route('/process_form', methods=['POST'])
+def process_form():
+    query = request.form.get('query')
+    if query is not None:
+        # Do something with query if necessary
+        return jsonify({"response": "bar"})
+    else:
+        return jsonify({"error": "query field is missing"}), 400
---
+++ next_app/app/page.js
@@ -0,0 +1,12 @@
+import Image from 'next/image'
+import styles from './page.module.css'
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <div>
+        <h1>Sup.</h1>
+      </div>
+    </main>
+  )
+}
